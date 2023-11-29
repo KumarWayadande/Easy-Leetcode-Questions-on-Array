bd4075a06e1b802081a746542ddc3f6f0b82004d
Kumar Wayadande <kumarwaidande7219@gmail.com>
2023-11-29 23:36:22 +0530

    Stock price calculation program developed

--- stock_buy_day.cpp
+++ stock_buy_day.cpp
@@ -1,56 +1,67 @@
-// #include<iostream>
-// #include<vector>
+// #include <vector>
+// #include <iostream>
 // using namespace std;
 
-// class Solution {
-
-//     public:
-//             int maxProfit(vector<int> & prices) {
-//                 int lowPriceIndex = 0, lowPrice = prices[0], highPrice;
-            
-//                 for(int i = 1; i < prices.size() - 1; i++){
-//                     if(prices[i] < lowPrice){
-//                         lowPrice = prices[i];
-//                         lowPriceIndex = i;
-//                     }
+// class Solution
+// {
+// public:
+//     int maxProfit(vector<int> &prices)
+//     {
+//         int profit = 0;
+//         int buyPrice, currentProfit, prevBuyPrice = prices[0];
+//         int count = 0;
+//         for (int i = 0; i < (prices.size() - 1); i++)
+//         {
+//             buyPrice = prices[i];
+//             if (buyPrice <= prevBuyPrice){
+//                 cout << endl << "Iteration : " << (count + 1);
+//                 count++;
+//                 for (int j = (i + 1); j < prices.size(); j++)
+//                 {
+//                     if ((prices[j] - buyPrice) > profit)
+//                         profit = (prices[j] - buyPrice);
 //                 }
-
-
-//                 highPrice = lowPrice;
-
-//                 for(int i = lowPriceIndex; i < prices.size() - 1; i++){
-//                     if(highPrice < prices[i + 1]){
-//                         highPrice = prices[i + 1];
-//                     }
-//                 }
-
-//                 // if(lowPrice == 0 && highPrice == 1)
-//                 //     return (highPrice - lowPrice);
-//                 // else
-//                 //     if(lowPrice == 0)
-//                 //         return ((highPrice - lowPrice) + 1);
-//                 //     else
-//                 //         return ((highPrice - lowPrice));
-
-//                 return (highPrice - lowPrice);
-
-//             }    
+//             }
+//             prevBuyPrice = prices[i];
+//         }
+//         return (profit <= 0 ? 0 : profit);
+//     }
 // };
 
+// int main()
+// {
 
-// int main(){
+//     vector<int> stockPrices = {7,1,5,3,6,4};
+
+//     // vector<int> stockPrices = {7,6,4,3,1};
 
 //     Solution s;
-//     vector<int> vec{3,2,6,5,0,3};
-//     {2,1,2,0,1};
-//     int profit = s.maxProfit(vec);
+//     // int profit = s.maxProfit(stockPrices);
 
-//     if(profit == 0){
-//         cout << "\nNo profit";
-//     }
-//     else{
-//         cout << "\nProfit Earned : " << profit;
-//     }
+//     // cout << "\nProfit : " << profit;
+//     cout << "\n Length : " << stockPrices.size();
 
 //     return 0;
 // }
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int lsf = INT_MAX;
+        int op = 0;
+        int pist = 0;
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(op < pist){
+                op = pist;
+            }
+        }
+        return op;
+    }
+};
