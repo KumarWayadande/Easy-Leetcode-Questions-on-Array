90ae77c9590b86372c72486a942b6b6025b7aca0
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-01-12 17:06:49 +0530

    max_consecutives_ones problem solved

---
+++ max_consecutives_one.cpp
@@ -0,0 +1,52 @@
+/*
+--------------------------------------
+Code Version : 1
+Author : Kumar Wayadande
+Upgraded Version ? No
+Test Cases : all test cases passed
+Worked in vscode ?  Yes
+--------------------------------------
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+class Solution {
+    public:
+        int findMaxConsecutiveOnes(vector<int>& nums) {
+                set<int> set1;
+                int large = 0;
+                for(int x : nums){
+                    if(x == 1)
+                        large++;
+                    else{
+                        set1.insert(set1.end(), large);
+                        large = 0;
+                    }
+                }
+
+                set1.insert(set1.end(), large);
+
+                return *--set1.end();
+        }
+};
+
+int main(){
+
+    vector<int> num = {0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0};
+    Solution s;
+    cout << "\n Max consecutive ones are : " << s.findMaxConsecutiveOnes(num);
+
+    return 0;
+}
+
+
+/*
+--------------------------------------
+End of code version 1
+All test cases passed
+--------------------------------------
+*/
