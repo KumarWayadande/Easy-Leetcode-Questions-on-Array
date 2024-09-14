c886bc59c448c094b8900c091f5a685e32a772e1
Kumar Wayadande <109848479+KumarWayadande@users.noreply.github.com>
2024-09-14 23:48:29 +0530

    Add files via upload

---
+++ min_rotated_array.cpp
@@ -0,0 +1,7 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         auto smallest = min_element(nums.begin(), nums.end());
+//         return *smallest;
+//     }
+// };
